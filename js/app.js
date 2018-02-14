d9702234efdd0b8d3cd9b1d511beb19661adb39a
Alex <Alex-Carver@.noreply.github.com>
2018-02-14 17:35:56 -0600

    fixed resize bug

--- js/app.js
+++ js/app.js
@@ -13,20 +13,3 @@ $(document).ready(function(){
     }); 
 });
 
-$(function() { var logo = $(".lrg-logo"); $(window).scroll(function() {
-var scroll = $(window).scrollTop();
-
-    if (scroll >= 500) {
-      if(!logo.hasClass("sml-logo")) {
-        logo.hide();
-        logo.removeClass('lrg-logo').addClass("sml-logo").fadeIn( "slow");
-      }
-    } else {
-      if(!logo.hasClass("lrg-logo")) {
-        logo.hide();
-        logo.removeClass("sml-logo").addClass('lrg-logo').fadeIn( "slow");
-      }
-    }
-
-});
-});
