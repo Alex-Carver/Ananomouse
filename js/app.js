cfd221eba73851d0ca21bd117f76f6555fa53db1
Alex <Alex-Carver@.noreply.github.com>
2018-04-11 16:04:40 -0500

    added btn link

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     });
 });
 
-$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-    var target = this.href.split('#');
-    $('.nav a').filter('a[href="#'+target[1]+'"]').tab('show');
-})
+$('#gotonew').click(function(){
+  $('a[href="#whatsNew"]').click();
+  console.log($('a[href="#whatsNew"]').text() + ' click triggered');
+});
