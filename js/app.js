1df622410fd908ac2861bc7886652c9cfbea6dbe
Alex <Alex-Carver@.noreply.github.com>
2018-04-16 16:27:41 -0500

    data file

--- js/app.js
+++ js/app.js
@@ -15,17 +15,7 @@ $(document).ready(function(){
     }); 
 });
 
-//Tabs Section5
-$(document).ready(function() {
-    $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
-        e.preventDefault();
-        $(this).siblings('a.active').removeClass("active");
-        $(this).addClass("active");
-        var index = $(this).index();
-        $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
-        $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
-    });
-});
+
 
 $('#gotonew').click(function(){
   $('a[href="#whatsNew"]').click();
