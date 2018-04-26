9f2aecd74678ecd7cce328d262deaef0e4c5e355
Alex <Alex-Carver@.noreply.github.com>
2018-04-26 13:54:39 -0500

    nav tabs scroll to top

--- js/app.js
+++ js/app.js
@@ -19,10 +19,23 @@ $(document).ready(function(){
 
 $('#gotonew').click(function(){
   $('a[href="#whatsNew"]').click();
+   $("html, body").animate({ scrollTop: 0 }, 600); 
+    return false; 
   console.log($('a[href="#whatsNew"]').text() + ' click triggered');
 });
+
+
+$('.nav-tabs a').click(function(){
+    $(this).tab('show');
+      $("html, body").animate({ scrollTop: 0 }, 600); 
+    return false; 
+})
+
+
+
 
 
 $('#video').click(function(){
    document.getElementById('video').play();
 });
+
