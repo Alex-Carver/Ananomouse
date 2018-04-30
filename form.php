22f0d388570cf6330865a43d6ecf0d1f22218e93
Alex <Alex-Carver@.noreply.github.com>
2018-04-30 14:34:48 -0500

    php

---
+++ form.php
@@ -0,0 +1,15 @@
+<?php
+    if (isset($_POST['s1'])) {
+      echo $_POST['name'];
+      echo $_POST['email'];
+
+      $myfile = fopen("data.txt", "w") or die("Unable to open file");
+      $name = $_POST['name']."\n";
+      fwrite($myfile, $name);
+
+      $email = $_POST['email']."\n";
+      fwrite($myfile, $email);
+      fclose($myfile);
+      echo “<h1>You data has been saved in a text file!</h1>”;
+    }
+  ?>
