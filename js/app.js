5aa9366655f2fb43c90ff52b816af3fbddb9bbed
Alex <Alex-Carver@.noreply.github.com>
2018-05-01 00:54:41 -0500

    animtions

--- js/app.js
+++ js/app.js
@@ -39,3 +39,12 @@ $('#video').click(function(){
    document.getElementById('video').play();
 });
 
+wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+})
+wow.init();
+        
