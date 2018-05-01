e4ee941fa4955134c4536e2583770e9a6c875ec0
Alex <Alex-Carver@.noreply.github.com>
2018-05-01 13:13:52 -0500

    final deliverable

--- js/app.js
+++ js/app.js
@@ -1,50 +1,39 @@
-
-
 //Sticky Back to Top button
-$(document).ready(function(){ 
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 800) { 
-            $('#scroll').fadeIn(); 
-        } else { 
-            $('#scroll').fadeOut(); 
-        } 
-    }); 
-    $('#scroll').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    }); 
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 800) {
+      $("#scroll").fadeIn();
+    } else {
+      $("#scroll").fadeOut();
+    }
+  });
+  $("#scroll").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
 });
 
 
-
-$('#gotonew').click(function(){
+//Features btn click function to What's New page
+$("#gotonew").click(function() {
   $('a[href="#whatsNew"]').click();
-   $("html, body").animate({ scrollTop: 0 }, 600); 
-    return false; 
-  console.log($('a[href="#whatsNew"]').text() + ' click triggered');
+  $("html, body").animate({ scrollTop: 0 }, 600);
+  return false;
+  console.log($('a[href="#whatsNew"]').text() + " click triggered");
+});
+
+//Nav tabs return to top on click function
+$(".nav-tabs1 a").click(function() {
+  $(this).tab("show");
+  $("html, body").animate({ scrollTop: 0 }, 600);
+  return false;
+});
+
+//Video play function
+$("#video").click(function() {
+  document.getElementById("video").play();
 });
 
 
-$('.nav-tabs1 a').click(function(){
-    $(this).tab('show');
-      $("html, body").animate({ scrollTop: 0 }, 600); 
-    return false; 
-})
 
-
-
-
-
-$('#video').click(function(){
-   document.getElementById('video').play();
-});
-
-wow = new WOW({
-    boxClass: 'wow', // default
-    animateClass: 'animated', // default
-    offset: 0, // default
-    mobile: true, // default
-    live: true // default
-})
-wow.init();
         
