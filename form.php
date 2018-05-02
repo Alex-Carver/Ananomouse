8df160e6be2611e39a314ce899b641a6ac754a75
Alex <Alex-Carver@.noreply.github.com>
2018-05-02 13:17:17 -0500

    added email script

--- form.php
+++ form.php
@@ -1,14 +1,26 @@
 <?php
     if (isset($_POST['s1'])) {
-      echo $_POST['name'];
-      echo $_POST['email'];
+      $to = "email@example.com"; // this is your Email address
+      $from = $_POST['email']; // this is the sender's Email address
+      $subject = "Form submission";//form subject
+      $subject2 = "Copy of your form submission";//copy of your form
+      echo $_POST['name'];//users name
+      echo $_POST['email'];//users email
 
-      $myfile = fopen("data.txt", "w") or die("Unable to open file");
-      $name = $_POST['name']."\n";
-      fwrite($myfile, $name);
+      $headers = "From:" . $from;//your email
+      $headers2 = "From:" . $to;//users email
+      mail($to,$subject,$message,$headers);//sends mail
+      mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+      echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";//users name and message
 
-      $email = $_POST['email']."\n";
-      fwrite($myfile, $email);
-      fclose($myfile);
+      $myfile = fopen("data.txt", "w") or die("Unable to open file");//opens data file
+      $name = $_POST['name']."\n";// call name 
+      fwrite($myfile, $name);//writes name
+
+      $email = $_POST['email']."\n";//call email
+      fwrite($myfile, $email);//write email
+      fclose($myfile);//close file and save
     }
   ?>
+
+
