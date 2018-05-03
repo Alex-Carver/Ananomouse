f5fbab0919562efc9b266397ac928d1caf4dfe19
Alex <Alex-Carver@.noreply.github.com>
2018-05-03 14:49:02 -0500

    thankyou page and email script

--- form.php
+++ form.php
@@ -1,18 +1,18 @@
 <?php
+header('Location: thankyou.html');
+exit();
     if (isset($_POST['s1'])) {
-      $to = "email@example.com"; // this is your Email address
+      $to = "acarver@advancedmidwestir.com"; // this is your Email address
       $from = $_POST['email']; // this is the sender's Email address
       $subject = "Form submission";//form subject
-      $subject2 = "Copy of your form submission";//copy of your form
       echo $_POST['name'];//users name
       echo $_POST['email'];//users email
 
       $headers = "From:" . $from;//your email
       $headers2 = "From:" . $to;//users email
       mail($to,$subject,$message,$headers);//sends mail
-      mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-      echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";//users name and message
-
+    
+  
       $myfile = fopen("data.txt", "w") or die("Unable to open file");//opens data file
       $name = $_POST['name']."\n";// call name 
       fwrite($myfile, $name);//writes name
@@ -22,5 +22,4 @@
       fclose($myfile);//close file and save
     }
   ?>
-
 
